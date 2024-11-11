9a629de5e69a18d4b6215ea5f71e67953e1f7a7f
Chris Buckley <147959807+cbuckley-code@users.noreply.github.com>
2024-11-10 18:14:10 -0800

    removing solana utils

--- src/components/aside/wallet-details.tsx
+++ src/components/aside/wallet-details.tsx
@@ -2,21 +2,9 @@ import { motion } from 'framer-motion';
 import { variants } from './aside-trends';
 import {UserAvatar} from '@components/user/user-avatar';
 import React from 'react';
-import {getProvider} from '@lib/solana-utils';
 import {Button} from '@components/ui/button';
 
 export function WalletDetails(): JSX.Element {
-
-    const provider = getProvider();
-
-    const handleConnect = async() => {
-        try {
-            const resp = await provider?.connect();
-            console.log(resp?.publicKey.toString());
-        } catch (err) {
-            console.log("we had an issue connecting to Phantom ", err);
-        }
-    }
 
     return (
         <section className='hover-animation rounded-2xl bg-main-sidebar-background'>
@@ -30,7 +18,6 @@ export function WalletDetails(): JSX.Element {
                         className='accent-tab absolute right-4 -translate-y-[72px] bg-main-accent text-lg font-bold text-white
                        outline-none transition hover:brightness-90 active:brightness-75 xs:static xs:translate-y-0
                        xs:hover:bg-main-accent/90 xs:active:bg-main-accent/75 xl:w-11/12'
-                        onClick={handleConnect}
                     >
                         Connect to Phantom
                     </Button>
